00badb41f7785e9f6dcfb0429deab783ca64a53a
Matthias Klan <matthias.klan@gmail.com>
2014-08-26 12:41:52 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,48 @@
+/*The MIT License (MIT)
+
+Copyright (c) 2014 Matthias Klan
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.*/
+
+function onClickHandler(info) {
+
+  var sText = info.selectionText
+  var date = Date.parse(sText);
+
+  if(date == null){
+    window.open("https://www.google.com/calendar/render?action=TEMPLATE&text="+sText+"&sf=true&output=xml");
+  }else{
+    var start = date.toISOString();
+    var end = date.add(1).hour().toISOString(); // one hour later
+    
+    start = start.replace(/(-|:|\.)/g, "").slice(0, -4) + 'Z';
+    end = end.replace(/(-|:|\.)/g, "").slice(0, -4) + 'Z';
+    
+    window.open("https://www.google.com/calendar/render?action=TEMPLATE&dates="+start+"/"+end+"&sf=true&output=xml");
+  }
+};
+
+chrome.contextMenus.onClicked.addListener(onClickHandler);
+
+// Set up context menu at install time.
+chrome.runtime.onInstalled.addListener(function() {
+  var context = "selection";
+  var title = "Create a new calendar entry";
+  var id = chrome.contextMenus.create({"title": title, "contexts":[context], "id": "context" + context});  
+});
